8d6cce5b4ef4eb0cfb4912676a8bd38ca39aeabe
hamahasan441-png <hamahasan441@gmail.com>
2026-08-28 19:00:44 +0200

    d4rf: add sandbox assurance package

---
+++ core/sandbox/__init__.py
@@ -0,0 +1,5 @@
+"""Non-destructive sandbox assurance checks for authorized lab mode."""
+
+from .assurance import AssuranceFinding, SandboxAssuranceReport, inspect_sandbox
+
+__all__ = ["AssuranceFinding", "SandboxAssuranceReport", "inspect_sandbox"]
